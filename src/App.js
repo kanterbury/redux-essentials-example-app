8525dbb82e913d7c804e0fdc88539e0fc52b098a
kanterbury <ksuzuki.db@gmail.com>
2020-09-18 13:16:53 +0900

    tutorial end

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ import { AddPostForm } from './features/posts/AddPostForm'
 import { PostsList } from './features/posts/PostsList'
 import { SinglePostPage } from './features/posts/SinglePostPage';
 import { EditPostForm } from './features/posts/EditPostForm';
+import { UsersList } from "./features/users/UsersList";
+import { UserPage } from "./features/users/UserPage";
+import { NotificationsList } from './features/notifications/NotificationsList'
+
+
 
 function App() {
   return (
@@ -18,6 +23,7 @@ function App() {
       <Navbar />
       <div className="App">
         <Switch>
+          <Route exact path="/notifications" component={NotificationsList} />
           <Route
             exact
             path="/"
@@ -30,6 +36,8 @@ function App() {
           />
           <Route exact path="/posts/:postId" component={SinglePostPage} />
           <Route exact path="/editPost/:postId" component={EditPostForm} />
+          <Route exact path="/users" component={UsersList} />
+          <Route exact path="/users/:userId" component={UserPage} />
           <Redirect to="/" />
         </Switch>
       </div>
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,16 +1,29 @@
-import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk, createSelector, nanoid, createEntityAdapter } from '@reduxjs/toolkit';
 import { client } from "../../api/client";
+import { parseISO } from 'date-fns'
 
-const initialState = {
-    posts: [],
+const postsAdapter = createEntityAdapter({
+    sortComparer: (a, b) => b.date.localeCompare(a.date)
+})
+
+const initialState = postsAdapter.getInitialState({
     status: 'idle',
     error: null
-  }
+})
 
 export const fetchPosts = createAsyncThunk('posts/fetchPosts', async() => {
     const response = await client.get('/fakeApi/posts')
     return response.posts
 })
+
+export const addNewPost = createAsyncThunk(
+    'posts/addNewPost',
+    // The payload creator receives the partial `{title, content, user}` object
+    async initialPost => {
+      const response = await client.post('/fakeApi/posts', { post: initialPost })
+      return response.post
+    }
+  )
 
 const postsSlice = createSlice({
     name: 'posts',
@@ -18,37 +31,14 @@ const postsSlice = createSlice({
     reducers: {
         reactionAdded(state, action){
             const { postId, reaction } = action.payload
-            const existingPost = state.posts.find(post => post.id === postId)
+            const existingPost = state.entities[postId]
             if (existingPost) {
-                existingPost.reactions[reaction]++
+                existingPost.reactions[reaction]　+= 1
             }
         },
-        postAdded: {
-            reducer(state, action) {
-                state.posts.push(action.payload)
-            },
-            prepare(title, content, userId){
-                return {
-                    payload: {
-                      id: nanoid(),
-                      date: new Date().toISOString(),
-                      title,
-                      content,
-                      user: userId,
-                      reactions: {
-                        thumbsUp: 0,
-                        hooray: 0,
-                        heart: 0,
-                        rocket: 0,
-                        eyes: 0,
-                      },
-                    }
-                }
-            }
-        },  
         postUpdated(state, action) {
             const { id,title, content } = action.payload
-            const existingPost = state.posts.find(post => post.id === id)
+            const existingPost = state.entities[id]
             if(existingPost) {
                 existingPost.title = title
                 existingPost.content = content
@@ -56,15 +46,14 @@ const postsSlice = createSlice({
         }
     },
     extraReducers: {
+        [addNewPost.fulfilled]: postsAdapter.addOne,
         [fetchPosts.pending]: (state, action) => {
             state.status = 'loading'
         },
         [fetchPosts.fulfilled]: (state, action) => {
             state.status = 'succeeded'
-            // Add any fetched posts to the array
-            // state.posts = state.posts.concat(action.payload)
-            state.posts = action.payload
-          },
+            postsAdapter.upsertMany(state, action.payload)
+        },
           [fetchPosts.rejected]: (state, action) => {
             state.status = 'failed'
             state.error = action.error.message
@@ -76,7 +65,15 @@ export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions;
 
 export default postsSlice.reducer;
 
-export const selectAllPosts = state => state.posts.posts;
+// Export the customized selectors for this adapter using `getSelectors`
+export const {
+    selectAll: selectAllPosts,
+    selectById: selectPostById,
+    selectIds: selectPostIds
+    // Pass in a selector that returns the posts slice of state
+  } = postsAdapter.getSelectors(state => state.posts)
 
-export const selectPostById = (state, postId) =>
-  state.posts.posts.find(post => post.id === postId)
+export const selectPostsByUser = createSelector(
+    [selectAllPosts, (state, userId) => userId],
+    (posts, userId) => posts.filter(post => post.user === userId)
+)
